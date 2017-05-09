fceb949fbf5a23a2d1465e24f240cac7dcde39cd
alvahro <alvahro@users.noreply.github.com>
2017-05-09 16:31:50 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-$('a[href=#about]').click(function (event)
-{
-event.stopPropagation();
-var Position = jQuery('[id="about"]').offset().top;
-jQuery('html, body').animate({ scrollTop: Position }, 1100);
-return false;
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous">
+</script>
+
+$('a[href=#about]').click(function (event) {
+  event.stopPropagation();
+  var Position = jQuery('[id="about"]').offset().top;
+  jQuery('html, body').animate({ scrollTop: Position }, 1100);
+  return false;
 });
